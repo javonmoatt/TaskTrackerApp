3b24d8d1f5caf436116debaf6d773da3ed44b0ff
Javon Moatt <javon@fourfronttech.com>
2022-08-13 11:45:53 -0500

    Services creation

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 
@@ -8,11 +9,17 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    // this.tasks = this.taskService.getTask();
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+  
+  deleteTask(task:Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id! !== task.id)));
   }
 
 }
